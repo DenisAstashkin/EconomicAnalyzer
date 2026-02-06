e7c246c8d637b3d92021b384590b3bd3697a7b68
DenisAstashkin <ast.den27@gmail.com>
2026-02-07 00:30:49 +0400

    Добавлена модель свечи и парсер свечи с МОСБИРЖИ

---
+++ src/models/candle.py
@@ -0,0 +1,12 @@
+class Candle:
+    def __init__(
+        self, min: str, 
+        max: str,
+        start: str,
+        close: str
+        ):
+        
+        self.min = min
+        self.max = max
+        self.start = start
+        self.close = close        
---
+++ src/utils/parser.py
@@ -0,0 +1,54 @@
+import requests
+from src.models import Candle
+
+def get_TMI(ticker: str) -> str:
+    url = f'https://iss.moex.com/iss/securities.json'
+    
+    params = {
+        'q': ticker
+    }
+
+    response = requests.get(url, params=params)
+    
+    data = response.json()
+    
+    data = data['securities']['data']
+    
+    if len(data) == 0:
+        raise Exception('[ERROR] Asset type not found')
+    
+    for i in data:
+        if i[0] != 'null' and i[0] == ticker:
+            return i[12]
+    
+    raise Exception('[ERROR] Asset type not found')
+    
+    
+def get_candles(ticker: str, type_ticker: str, interval: int, start: str, end: str) -> list[Candle]:
+    url = f'https://iss.moex.com/iss/engines/stock/markets/{type_ticker}/securities/{ticker}/candles.json'
+
+    params = {
+        'interval': interval,
+        'from': start,
+        'till': end
+    }
+    
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    data = data['candles']['data']
+    
+    if len(data) == 0:
+        raise Exception('[ERROR] Information about candles was not found')
+
+    candles = []
+    
+    for i in data:
+        candle = Candle(i[3], i[2], i[0], i[1])
+        
+        candles.append(candle)
+    
+    if len(candles) == 0:
+        raise Exception('[ERROR] Information about candles was not found')
+    
+    return candles
