70a54a583d1406040ba1ac4504410587d516e50c
DenisAstashkin <ast.den27@gmail.com>
2026-02-08 13:57:42 +0400

    Добавление информации к функциям и обновление некоторых

--- src/utils/parser.py
+++ src/utils/parser.py
@@ -2,6 +2,19 @@ import requests
 from src.models import Candle
 
 def get_TMI(ticker: str) -> str:
+    """ Функция для сопоставления нейминга ценной бумаги с её типом (акция, облигация и тд)
+
+    Args:
+        ticker (str): Нейминг ценной бумаги
+
+    Raises:
+        Exception: Ошибка ненайденных данных
+        Exception: Ошибка ненайденного типа бумаги
+
+    Returns:
+        str: Тип бумаги
+    """
+    
     url = f'https://iss.moex.com/iss/securities.json'
     
     params = {
@@ -25,6 +38,22 @@ def get_TMI(ticker: str) -> str:
     
     
 def get_candles(ticker: str, type_ticker: str, interval: int, start: str, end: str) -> list[Candle]:
+    """ Функция для парсинга данных свечевого графика для определённой бумаги
+
+    Args:
+        ticker (str): Нейминг ценной бумаги
+        type_ticker (str): Тип ценной бумаги
+        interval (int): Таймфрейм
+        start (str): Дата начала парсинга (ГГГГ-ММ-ДД ЧЧ:ММ:СС)
+        end (str): Дата окончания парсинга (ГГГГ-ММ-ДД ЧЧ:ММ:СС)
+
+    Raises:
+        Exception: Ошибка получения свечевого графика
+
+    Returns:
+        list[Candle]: Список свеч
+    """
+    
     url = f'https://iss.moex.com/iss/engines/stock/markets/{type_ticker}/securities/{ticker}/candles.json'
 
     params = {
@@ -47,8 +76,5 @@ def get_candles(ticker: str, type_ticker: str, interval: int, start: str, end: s
         candle = Candle(i[3], i[2], i[0], i[1])
         
         candles.append(candle)
-    
-    if len(candles) == 0:
-        raise Exception('[ERROR] Information about candles was not found')
     
     return candles
